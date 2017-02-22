e3a9f007eef3c92559d093019a1b0e6532d0def4
Ted Spence <ted.spence@avalara.com>
2017-02-22 11:55:41 -0800

    Cleaned up projects, moved ErrorResults to the root, and added extensions to the transaction builder

---
+++ src/ErrorTargetCode.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Avalara.AvaTax.RestClient
+{
+    /// <summary>
+    /// ErrorTargetCode
+    /// </summary>
+    public enum ErrorTargetCode
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        Unknown,
+
+        /// <summary>
+        /// 
+        /// </summary>
+        HttpRequest,
+
+        /// <summary>
+        /// 
+        /// </summary>
+        HttpRequestHeaders,
+
+        /// <summary>
+        /// 
+        /// </summary>
+        IncorrectData,
+
+        /// <summary>
+        /// 
+        /// </summary>
+        AvaTaxApiServer,
+
+        /// <summary>
+        /// 
+        /// </summary>
+        AvalaraIdentityServer,
+
+        /// <summary>
+        /// 
+        /// </summary>
+        CustomerAccountSetup,
+
+    }
+}
--- src/TransactionBuilder.cs
+++ src/TransactionBuilder.cs
@@ -147,8 +147,8 @@ namespace Avalara.AvaTax.RestClient
         /// <returns></returns>
         public TransactionBuilder WithAddress(TransactionAddressType type, string line1, string line2, string line3, string city, string region, string postalCode, string country)
         {
-            if (_model.addresses == null) _model.addresses = new Dictionary<TransactionAddressType, AddressInfo>();
-            var ai = new AddressInfo
+            if (_model.addresses == null) _model.addresses = new AddressesModel();
+            var ai = new AddressLocationInfo
             {
                 line1 = line1,
                 line2 = line2,
@@ -158,8 +158,55 @@ namespace Avalara.AvaTax.RestClient
                 postalCode = postalCode,
                 country = country
             };
-            _model.addresses[type] = ai;
+            SetAddress(_model.addresses, type, ai);
             return this;
+        }
+
+        /// <summary>
+        /// Add an address to this transaction using an existing company location code.
+        /// 
+        /// AvaTax will search for a company location whose code matches the `locationCode` parameter and use the address
+        /// of that location.
+        /// </summary>
+        /// <param name="type">Address Type. Can be ShipFrom, ShipTo, PointOfOrderAcceptance, PointOfOrderOrigin, SingleLocation.</param>
+        /// <param name="locationCode">The location code of the location to use as this address.</param>
+        /// <returns></returns>
+        public TransactionBuilder WithAddress(TransactionAddressType type, string locationCode)
+        {
+            if (_model.addresses == null) _model.addresses = new AddressesModel();
+            var ai = new AddressLocationInfo
+            {
+                locationCode = locationCode
+            };
+            SetAddress(_model.addresses, type, ai);
+            return this;
+        }
+
+        /// <summary>
+        /// Set a specific address type for an address object
+        /// </summary>
+        /// <param name="addresses"></param>
+        /// <param name="type"></param>
+        /// <param name="ai"></param>
+        public static void SetAddress(AddressesModel addresses, TransactionAddressType type, AddressLocationInfo ai)
+        {
+            switch (type) {
+                case TransactionAddressType.PointOfOrderAcceptance:
+                    addresses.pointOfOrderAcceptance = ai;
+                    break;
+                case TransactionAddressType.PointOfOrderOrigin:
+                    addresses.pointOfOrderOrigin = ai;
+                    break;
+                case TransactionAddressType.ShipFrom:
+                    addresses.shipFrom = ai;
+                    break;
+                case TransactionAddressType.ShipTo:
+                    addresses.shipTo = ai;
+                    break;
+                case TransactionAddressType.SingleLocation:
+                    addresses.singleLocation = ai;
+                    break;
+            }
         }
 
         /// <summary>
@@ -171,12 +218,13 @@ namespace Avalara.AvaTax.RestClient
         /// <returns></returns>
         public TransactionBuilder WithLatLong(TransactionAddressType type, decimal latitude, decimal longitude)
         {
-            var ai = new AddressInfo
+            if (_model.addresses == null) _model.addresses = new AddressesModel();
+            var ai = new AddressLocationInfo
             {
                 latitude = latitude,
                 longitude = longitude
             };
-            _model.addresses[type] = ai;
+            SetAddress(_model.addresses, type, ai);
             return this;
         }
 
@@ -195,8 +243,8 @@ namespace Avalara.AvaTax.RestClient
         public TransactionBuilder WithLineAddress(TransactionAddressType type, string line1, string line2, string line3, string city, string region, string postalCode, string country)
         {
             var line = GetMostRecentLine("WithLineAddress");
-            if (line.addresses == null) line.addresses = new Dictionary<TransactionAddressType, AddressInfo>();
-            line.addresses[type] = new AddressInfo
+            if (line.addresses == null) line.addresses = new AddressesModel();
+            var ai = new AddressLocationInfo
             {
                 line1 = line1,
                 line2 = line2,
@@ -206,6 +254,7 @@ namespace Avalara.AvaTax.RestClient
                 postalCode = postalCode,
                 country = country
             };
+            SetAddress(line.addresses, type, ai);
             return this;
         }
 
@@ -269,16 +318,22 @@ namespace Avalara.AvaTax.RestClient
         /// </summary>
         /// <param name="amount">Value of the item.</param>
         /// <param name="quantity">Quantity of the item.</param>
-        /// <param name="taxCode">Tax Code of the item. If left blank, the default item (P0000000) is assumed.</param>
+        /// <param name="taxCode">Tax Code of the item. If left blank, the default item (P0000000) is assumed.  Use ListTaxCodes() for a list of values.</param>
+        /// <param name="customerUsageType">The intended usage type for this line.  Use ListEntityUseCodes() for a list of values.</param>
+        /// <param name="description">A friendly description of this line item.</param>
+        /// <param name="itemCode">The item code of this item in your product item definitions.</param>
         /// <returns></returns>
-        public TransactionBuilder WithLine(decimal amount, decimal quantity = 1, string taxCode = null)
+        public TransactionBuilder WithLine(decimal amount, decimal quantity = 1, string taxCode = null, string description = null, string itemCode = null, string customerUsageType = null)
         {
             var l = new LineItemModel
             {
                 number = _line_number.ToString(),
                 quantity = quantity,
                 amount = amount,
-                taxCode = taxCode
+                taxCode = taxCode,
+                description = description,
+                itemCode = itemCode,
+                customerUsageType = customerUsageType
             };
 
             _model.lines.Add(l);
@@ -307,12 +362,12 @@ namespace Avalara.AvaTax.RestClient
             {
                 number = _line_number.ToString(),
                 quantity = 1,
-                amount = amount
+                amount = amount,
+                addresses = new AddressesModel()
             };
 
             // Add this address
-            l.addresses = new Dictionary<TransactionAddressType, AddressInfo>();
-            l.addresses[type] = new AddressInfo
+            var ai = new AddressLocationInfo
             {
                 line1 = line1,
                 line2 = line2,
@@ -322,6 +377,7 @@ namespace Avalara.AvaTax.RestClient
                 postalCode = postalCode,
                 country = country
             };
+            SetAddress(l.addresses, type, ai);
 
             // Put this line in the model
             _model.lines.Add(l);
@@ -399,6 +455,19 @@ namespace Avalara.AvaTax.RestClient
         public TransactionModel Create()
         {
             return _client.CreateTransaction(_model);
+        }
+
+        /// <summary>
+        /// Return the current CreateTransactionModel but do not call AvaTax to record the transaction.
+        /// 
+        /// If you would like to perform additional modifications to this object beyond the fluent 
+        /// interface, you can use this function to return the currently assembled model and then make
+        /// additional changes to it.
+        /// </summary>
+        /// <returns></returns>
+        public CreateTransactionModel GetCreateTransactionModel()
+        {
+            return _model;
         }
 #endif
 
