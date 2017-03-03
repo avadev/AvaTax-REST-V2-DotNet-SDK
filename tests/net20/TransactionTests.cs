5639e52c36b2a0d35ce128ab640a83ef4cdd8818
Zhenya Frolov <zhenya.frolov@avalara.com>
2017-03-03 15:04:33 -0800

    Added an independent TaxOverride test.

--- tests/net20/TransactionTests.cs
+++ tests/net20/TransactionTests.cs
@@ -1,8 +1,6 @@
 ﻿using Avalara.AvaTax.RestClient;
 using NUnit.Framework;
 using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace Tests.Avalara.AvaTax.RestClient.net20
 {
@@ -20,11 +18,12 @@ namespace Tests.Avalara.AvaTax.RestClient.net20
         [SetUp]
         public void Setup()
         {
-            try {
+            try
+            {
                 // Create a client and set up authentication
-                Client = new AvaTaxClient(typeof(TransactionTests).Assembly.FullName, 
-                    typeof(TransactionTests).Assembly.GetName().Version.ToString(), 
-                    Environment.MachineName, 
+                Client = new AvaTaxClient(typeof(TransactionTests).Assembly.FullName,
+                    typeof(TransactionTests).Assembly.GetName().Version.ToString(),
+                    Environment.MachineName,
                     AvaTaxEnvironment.Sandbox)
                     .WithSecurity(Environment.GetEnvironmentVariable("SANDBOX_USERNAME"), Environment.GetEnvironmentVariable("SANDBOX_PASSWORD"));
 
@@ -61,8 +60,9 @@ namespace Tests.Avalara.AvaTax.RestClient.net20
                 Assert.True(TestCompany.locations.Count > 0, "Test company should have locations");
 
                 // Shouldn't fail
-            } catch (Exception ex) {
-                Assert.Fail("Exception in SetUp: " + ex.ToString());
+            } catch (Exception ex)
+            {
+                Assert.Fail("Exception in SetUp: " + ex);
             }
         }
 
@@ -73,7 +73,8 @@ namespace Tests.Avalara.AvaTax.RestClient.net20
         [TearDown]
         public void TearDown()
         {
-            try {
+            try
+            {
 
                 // Re-fetch the company
                 var company = Client.GetCompany(TestCompany.id, null);
@@ -87,8 +88,9 @@ namespace Tests.Avalara.AvaTax.RestClient.net20
                 Assert.False(disableResult.isActive, "Company should have been deactivated");
 
                 // Shouldn't fail
-            } catch (Exception ex) {
-                Assert.Fail("Exception in TearDown: " + ex.ToString());
+            } catch (Exception ex)
+            {
+                Assert.Fail("Exception in TearDown: " + ex);
             }
         }
         #endregion
@@ -96,7 +98,6 @@ namespace Tests.Avalara.AvaTax.RestClient.net20
         /// <summary>
         /// To debug this application, call app must be called with args[0] as username and args[1] as password
         /// </summary>
-        /// <param name="args"></param>
         [Test]
         public void TransactionWorkflow()
         {
@@ -116,14 +117,14 @@ namespace Tests.Avalara.AvaTax.RestClient.net20
             Assert.True(transaction.lines.Count == 3, "Transaction should have three lines");
 
             // Now commit that transaction
-            var commitResult = Client.CommitTransaction(TestCompany.companyCode, transaction.code, new CommitTransactionModel() { commit = true });
+            var commitResult = Client.CommitTransaction(TestCompany.companyCode, transaction.code, new CommitTransactionModel { commit = true });
 
             // Ensure that this transaction was committed
             Assert.NotNull(commitResult, "Should have been able to call CommitTransaction");
             Assert.True(commitResult.status == DocumentStatus.Committed, "Transaction should have been committed");
 
             // Now void the transaction
-            var voidResult = Client.VoidTransaction(TestCompany.companyCode, transaction.code, new VoidTransactionModel()
+            var voidResult = Client.VoidTransaction(TestCompany.companyCode, transaction.code, new VoidTransactionModel
             {
                 code = VoidReasonCode.DocVoided
             });
@@ -131,6 +132,45 @@ namespace Tests.Avalara.AvaTax.RestClient.net20
             // Ensure that the transaction was voided
             Assert.NotNull(voidResult, "Should have been able to call VoidTransactoin");
             Assert.True(voidResult.status == DocumentStatus.Cancelled, "Transaction should have been voided");
+        }
+
+        [Test]
+        public void TaxOverrideExample()
+        {
+            // Create base transaction.
+            var builder = new TransactionBuilder(Client, TestCompany.companyCode, DocumentType.SalesInvoice,
+                    "TaxOverrideCustomerCode")
+                .WithAddress(TransactionAddressType.SingleLocation, "521 S Weller St", null, null, "Seattle", "WA",
+                    "98104", "US")
+                .WithLine(100.0m, 1, "P0000000")
+                .WithLine(200m);
+
+            var transaction = builder.Create();
+
+            // Ensure this transaction was created.
+            Assert.NotNull(transaction, "Transaction should have been created");
+
+            // Add Line-level TaxOverride.
+            var overrideTransaction = builder
+                .WithLineTaxOverride(TaxOverrideType.TaxAmount, "Tax Override Reason", 1)
+                .Create();
+
+            // Ensure this transaction was created.
+            Assert.NotNull(overrideTransaction, "Transaction should have been created");
+
+            // Compare the two transactions.
+            Assert.AreEqual(overrideTransaction.totalTaxCalculated, transaction.totalTaxCalculated, "Total Tax Calculated should be the same.");
+            Assert.True(overrideTransaction.totalTax < transaction.totalTax, "Total Tax should not be the same. Overridden transaction should be smaller.");
+
+            // Compare the transaction lines.
+            var overrideLine = overrideTransaction.lines[1];
+            var line = transaction.lines[1];
+            Assert.AreEqual(overrideLine.isItemTaxable, line.isItemTaxable);
+            Assert.AreEqual(overrideLine.taxCalculated, line.taxCalculated);
+            Assert.AreEqual(overrideLine.lineAmount, line.lineAmount);
+            Assert.AreEqual(1, overrideLine.tax);
+            Assert.True(overrideLine.tax < line.tax);
+            Assert.AreEqual(TaxOverrideTypeId.TaxAmount, overrideLine.taxOverrideType);
         }
     }
 }
