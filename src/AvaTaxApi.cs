8d6021fabf01bd4e31da2f2eb999cc867b3083eb
Zhenya Frolov <zhenya.frolov@avalara.com>
2017-04-03 13:54:42 -0700

    Post SBX deploy update.

--- src/AvaTaxApi.cs
+++ src/AvaTaxApi.cs
@@ -16,7 +16,7 @@ using System.Threading.Tasks;
  * @author     Zhenya Frolov <zhenya.frolov@avalara.com>
  * @copyright  2004-2017 Avalara, Inc.
  * @license    https://www.apache.org/licenses/LICENSE-2.0
- * @version    2.17.4-59
+ * @version    2.17.4-60
  * @link       https://github.com/avadev/AvaTax-REST-V2-DotNet-SDK
  */
 
@@ -27,7 +27,7 @@ namespace Avalara.AvaTax.RestClient
         /// <summary>
         /// Returns the version number of the API used to generate this class
         /// </summary>
-        public static string API_VERSION { get { return "2.17.4-59"; } }
+        public static string API_VERSION { get { return "2.17.4-60"; } }
 
 #region Methods
 
