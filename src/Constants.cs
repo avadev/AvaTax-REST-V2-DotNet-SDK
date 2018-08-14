8a1083a793c21a068dfd2818b6bf3c4228d80934
Greg Hester <greg.hester@avalara.com>
2018-08-14 10:41:55 -0700

    Updated constants.

--- src/Constants.cs
+++ src/Constants.cs
@@ -15,7 +15,7 @@ namespace Avalara.AvaTax.RestClient
         public static readonly string AVALARA_CLIENT_HEADER = "X-Avalara-Client";
 
         /// <summary>
-        /// Header used when creating a transaction with the connectionId of the extractor.
+        /// Header used when creating a transaction with the dataSourceId of the data source.
         /// </summary>
         public static readonly string AVALARA_DATASOURCE_HEADER = "X-Avalara-DataSource";
 
