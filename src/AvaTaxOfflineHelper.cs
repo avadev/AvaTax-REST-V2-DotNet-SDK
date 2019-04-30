91791dcd88f0e49f2f500d67586691a81f206dae
Wayne Myer <wayne.myer@avalara.com>
2019-04-30 15:17:13 -0700

    Saving out work

---
+++ src/AvaTaxOfflineHelper.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Avalara.AvaTax.RestClient.net45
+{
+    /// <summary>
+    /// This class contains methods to assist with content for offline calculations. 
+    /// </summary>
+    public class AvaTaxOfflineHelper
+    {
+        /// <summary>Caches the content.</summary>
+        public static void CacheRateContent(AvaTaxClient client, string region, string zip)
+        {
+            //Call rate by ZIP endpoint.
+            var rateFile = client.TaxRatesByPostalCode(region, zip);
+
+            //Save the rate by ZIP file in the local ZIP folder.
+            string rateFileString = rateFile.ToString();
+        }
+
+        /// <summary>Verifies the local zip rate available.</summary>
+        /// <param name="zip">The zip.</param>
+        /// <returns></returns>
+        public static bool VerifyLocalZipRateAvailable(string zip)
+        {
+            return false;
+        }
+    }
+}
--- tests/net45/FreeTaxRatesTest.cs
+++ tests/net45/FreeTaxRatesTest.cs
@@ -1,4 +1,5 @@
 ﻿using Avalara.AvaTax.RestClient;
+using Avalara.AvaTax.RestClient.net45;
 using NUnit.Framework;
 using System;
 using System.Collections.Generic;
@@ -7,32 +8,63 @@ using System.Threading.Tasks;
 
 namespace Tests.Avalara.AvaTax.RestClient.netstandard
 {
-    class FreeTaxRatesTest
+    [TestFixture]
+    public class FreeTaxRatesTest
     {
+        public AvaTaxClient Client { get; set; }        
+
+        #region Setup / Teardown
+        /// <summary>
+        /// Create a company for use with these tests
+        /// </summary>
+        [SetUp]
+        public void Setup()
+        {
+            try {
+                // Create a client and set up authentication
+                Client = new AvaTaxClient(typeof(TransactionTests).Assembly.FullName,
+                    typeof(TransactionTests).Assembly.GetName().Version.ToString(),
+                    Environment.MachineName,
+                    AvaTaxEnvironment.Sandbox)
+                    .WithSecurity(Environment.GetEnvironmentVariable("SANDBOX_USERNAME"), Environment.GetEnvironmentVariable("SANDBOX_PASSWORD"));
+            } catch (Exception ex) {
+                Assert.Fail("Exception in SetUp: " + ex);
+            }
+        }        
+        #endregion
+
         [Test]
         public async Task FreeTaxRates()
         {
-            // Create a client and set up authentication
-            var client = new AvaTaxClient(typeof(TransactionTests).Assembly.FullName,
-                typeof(TransactionTests).Assembly.GetName().Version.ToString(),
-                Environment.MachineName,
-                AvaTaxEnvironment.Sandbox)
-                .WithSecurity(Environment.GetEnvironmentVariable("SANDBOX_USERNAME"), Environment.GetEnvironmentVariable("SANDBOX_PASSWORD"));
-
             // Call TaxRates for a few addresses and verify that the rates are nonzero
-            var tr = await client.TaxRatesByAddressAsync("123 Main Street", null, null, "Irvine", "CA", "92615", "US");
+            var tr = await Client.TaxRatesByAddressAsync("123 Main Street", null, null, "Irvine", "CA", "92615", "US");
             Assert.NotNull(tr);
             Assert.True(tr.totalRate > 0.05m);
 
             // Washington
-            tr = await client.TaxRatesByAddressAsync("100 Ravine Lane NE", null, null, "Bainbridge Island", "WA", "98110", "US");
+            tr = await Client.TaxRatesByAddressAsync("100 Ravine Lane NE", null, null, "Bainbridge Island", "WA", "98110", "US");
             Assert.NotNull(tr);
             Assert.True(tr.totalRate > 0.05m);
 
             // By postal code for New York
-            tr = await client.TaxRatesByPostalCodeAsync("US", "10010");
+            tr = await Client.TaxRatesByPostalCodeAsync("US", "10010");
             Assert.NotNull(tr);
             Assert.True(tr.totalRate > 0.05m);
+        }
+
+        /// <summary>
+        /// Test the local rate by ZIP caching.
+        /// </summary>        
+        [Test]
+        public async Task CacheRatesByZipTest()
+        {
+            //Call the content caching helper.
+            AvaTaxOfflineHelper.CacheRateContent(Client, "US", "12590");
+
+            //Verify that the file was stored locally. 
+
+
+            //Verify that the local file can be used for rate calculation.
         }
     }
 }
