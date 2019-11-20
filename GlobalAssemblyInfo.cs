6777a97d2d128690b3afbf8b1b8d9d37cbd30f6d
contygm <contygm@gmail.com>
2019-11-20 08:47:35 -0800

    test update

--- GlobalAssemblyInfo.cs
+++ GlobalAssemblyInfo.cs
@@ -30,5 +30,5 @@ using System.Runtime.InteropServices;
 //      Revision
 //
 
-[assembly: AssemblyVersion("19.11.0")]
-[assembly: AssemblyFileVersion("19.11.0")]
+[assembly: AssemblyVersion("19.10.0")]
+[assembly: AssemblyFileVersion("19.10.0")]
