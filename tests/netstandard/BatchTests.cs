ee6ae00c6a21c6a0a19dea8b1613e13d99aaddae
contygm <contygm@gmail.com>
2020-02-14 12:57:56 -0800

    20.1: updated net11 batch test

--- tests/netstandard/BatchTests.cs
+++ tests/netstandard/BatchTests.cs
@@ -184,9 +184,9 @@ namespace Tests.Avalara.AvaTax.RestClient.netstandard
                 // Alright. Time to download the sent batch file.
                 var fileResult = Client.DownloadBatch(TestCompany.id, batchFetchResult.id.Value, batchFetchResult.files[0].id.Value);
                 Assert.NotNull(fileResult);
-                
+
                 // Compare what we got back with what we sent.
-                Assert.AreEqual(batchFetchResult.name + ".Input.CSV", fileResult.Filename);
+                Assert.AreEqual(batchFetchResult.name + ".Input.CSV; filename*=UTF-8''" + batchFetchResult.name + ".Input.CSV", fileResult.Filename);
                 Assert.AreEqual(batchFileModel.content, fileResult.Data);
                 Assert.AreEqual(batchFileModel.contentType, fileResult.ContentType);
             } catch (AvaTaxError e)
