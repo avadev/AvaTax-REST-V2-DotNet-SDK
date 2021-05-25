9e84b7c53d16a909c7732b88ddee2222b7210b8c
Anshul Ameta <anshul.ameta@avalara.com>
2021-05-25 14:43:29 +0530

    21.3.3 update

---
+++ src/UserConfiguration.cs
@@ -0,0 +1,18 @@
+﻿namespace Avalara.AvaTax.netstandard11
+{
+    /// <summary>
+    /// Class for User configuration
+    /// </summary>
+    public class UserConfiguration
+    {
+        /// <summary>
+        /// Gets or sets timeout in minutes
+        /// </summary>
+        public int TimeoutInMinutes { get; set; } = 20;
+
+        /// <summary>
+        /// Gets or sets Maximum retry attempts
+        /// </summary>
+        public int MaxRetryAttempts { get; set; } 
+    }
+}
---
+++ tests/netstandard/ExceptionRetryTest.cs
@@ -0,0 +1,39 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace Tests.Avalara.AvaTax.netstandard11
+{
+    [TestFixture]
+    public class ExceptionRetryTest
+    {
+
+        [Test]
+        public void Exception_Retry_Zero_MaxAttempt()
+        {
+            int maxRetryAttempts = 0;
+            TestHelper testHelper = new TestHelper(maxRetryAttempts);
+            var result = testHelper.AddNonZeroIntegers(3, 0);
+            Assert.AreEqual(maxRetryAttempts+1, testHelper.MethodCount);
+        }
+
+        [Test]
+        public void Exception_Retry_Three_MaxAttempt()
+        {
+            int maxRetryAttempts = 3;
+            TestHelper testHelper = new TestHelper(maxRetryAttempts);
+            var result = testHelper.AddNonZeroIntegers(3, 0);
+            Assert.AreEqual(maxRetryAttempts + 1, testHelper.MethodCount);
+
+        }
+
+        [Test]
+        public void Aggregate_Exception_Not_Following_RetryPolicy()
+        {
+            int maxRetryAttempts = 3;
+            TestHelper testHelper = new TestHelper(maxRetryAttempts);
+            Assert.Throws<AggregateException>(()=>testHelper.DivideIntegers(3, 0));
+            Assert.AreEqual(1, testHelper.MethodCount);
+
+        }
+    }
+}
