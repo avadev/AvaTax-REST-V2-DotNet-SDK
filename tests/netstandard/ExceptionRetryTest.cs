e14c76da7890e4c33f5202613f29643e97853ef1
Anshul Ameta <anshul.ameta@avalara.com>
2021-05-31 18:33:51 +0530

    Test files updated in all versions

--- tests/netstandard/ExceptionRetryTest.cs
+++ tests/netstandard/ExceptionRetryTest.cs
@@ -1,27 +1,28 @@
-﻿using NUnit.Framework;
+﻿using Avalara.AvaTax.RestClient;
+using NUnit.Framework;
 using System;
+using System.Net.Http;
 
 namespace Tests.Avalara.AvaTax.RestClient.netstandard
 {
     [TestFixture]
     public class ExceptionRetryTest
     {
-
         [Test]
-        public void Exception_Retry_Zero_MaxAttempt()
+        public void MethodExecutesOnce_MaxRetryAttempt_Zero_Test()
         {
             int maxRetryAttempts = 0;
             TestHelper testHelper = new TestHelper(maxRetryAttempts);
-            var result = testHelper.AddNonZeroIntegers(3, 0);
-            Assert.AreEqual(maxRetryAttempts+1, testHelper.MethodCount);
+            Assert.ThrowsAsync<HttpRequestException>(async () => await testHelper.AddNonZeroIntegers(3, 0));
+            Assert.AreEqual(1, testHelper.MethodCount);
         }
 
         [Test]
-        public void Exception_Retry_Three_MaxAttempt()
+        public void MethodExecutes_FourTimes_MaxRetryAttempt_Three_Test()
         {
             int maxRetryAttempts = 3;
             TestHelper testHelper = new TestHelper(maxRetryAttempts);
-            var result = testHelper.AddNonZeroIntegers(3, 0);
+            Assert.ThrowsAsync<HttpRequestException>(async () => await testHelper.AddNonZeroIntegers(3, 0));
             Assert.AreEqual(maxRetryAttempts + 1, testHelper.MethodCount);
         }
 
@@ -30,8 +31,28 @@ namespace Tests.Avalara.AvaTax.RestClient.netstandard
         {
             int maxRetryAttempts = 3;
             TestHelper testHelper = new TestHelper(maxRetryAttempts);
-            Assert.Throws<AggregateException>(()=>testHelper.DivideIntegers(3, 0));
+            Assert.ThrowsAsync<DivideByZeroException>(async () => await testHelper.DivideIntegers(3, 0));
+            // As method is throwing DevideByZero Exception, method will execute only once even though maxRetryAttempt set to 3.
             Assert.AreEqual(1, testHelper.MethodCount);
+        }
+
+        [Test]
+        public void UserConfiguration_test_MaxRetryAttempt_Three()
+        {
+            UserConfiguration userConfiguration = new UserConfiguration { MaxRetryAttempts = 3 };
+            TestHelper testHelper = new TestHelper(userConfiguration);
+            Assert.ThrowsAsync<HttpRequestException>(async () => await testHelper.AddNonZeroIntegers(3, 0));
+            Assert.AreEqual(userConfiguration.MaxRetryAttempts + 1, testHelper.MethodCount);
+        }
+
+        [Test]
+        public void UserConfiguration_test_MaxRetryAttempt_Negative()
+        {
+            UserConfiguration userConfiguration = new UserConfiguration { MaxRetryAttempts = -1 };
+            TestHelper testHelper = new TestHelper(userConfiguration);
+            Assert.ThrowsAsync<HttpRequestException>(async () => await testHelper.AddNonZeroIntegers(3, 0));
+            Assert.AreEqual(0, userConfiguration.MaxRetryAttempts);
+            Assert.AreEqual(userConfiguration.MaxRetryAttempts + 1, testHelper.MethodCount);
         }
     }
 }
