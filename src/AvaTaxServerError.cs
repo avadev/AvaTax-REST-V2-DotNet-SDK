e3e3b7afd44381e560fbca4faabf3cd92629a3bb
dipti.jadhav <dipti.jadhav@avalara.com>
2021-07-14 13:35:20 +0530

    Checked in - initial commit

---
+++ src/AvaTaxServerError.cs
@@ -0,0 +1,20 @@
+using System.Net;
+
+namespace Avalara.AvaTax.RestClient
+{
+    /// <summary>
+    /// Class for handling Avatax server errors - InternalServerError, RequestTimeout
+    /// </summary>
+    public class AvaTaxServerError : AvaTaxError
+    {
+        /// <summary>
+        /// Initializes <see cref="AvaTaxServerError"/> class
+        /// </summary>
+        /// <param name="errorResult">Error Result</param>
+        /// <param name="statusCode">HTTP status code</param>
+        public AvaTaxServerError(ErrorResult errorResult, HttpStatusCode statusCode)
+            : base(errorResult, statusCode)
+        {
+        }
+    }
+}
