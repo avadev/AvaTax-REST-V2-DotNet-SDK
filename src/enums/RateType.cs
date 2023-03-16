3f78d59d26e5de1b1c01d4068123155583db6b31
svc-developer <svc-developer@avalara.com>
2023-03-16 17:41:25 +0000

    23.3.0

--- src/enums/RateType.cs
+++ src/enums/RateType.cs
@@ -3,13 +3,13 @@ using System;
 /*
  * AvaTax API Client Library
  *
- * (c) 2004-2019 Avalara, Inc.
+ * (c) 2004-2023 Avalara, Inc.
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  *
- * @author Genevieve Conty
- * @author Greg Hester
+ * @author Jonathan Wenger <jonathan.wenger@avalara.com>
+ * @author Sachin Baijal <sachin.baijal@avalara.com>
  * Swagger name: AvaTaxClient 
  */
 
