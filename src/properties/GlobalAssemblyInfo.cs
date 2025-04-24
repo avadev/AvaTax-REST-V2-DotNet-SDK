5c1310bf5c129ff52fc0bbc20058a4952ff21bf7
svc-developer <75763841+svc-developer@users.noreply.github.com>
2025-04-24 09:32:07 -0700

    Update GlobalAssemblyInfo.cs

--- src/properties/GlobalAssemblyInfo.cs
+++ src/properties/GlobalAssemblyInfo.cs
@@ -2,9 +2,6 @@ using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
 [assembly: AssemblyTitle("Avalara.AvaTax.RestClient")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
@@ -14,21 +11,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
-// The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("6ba93538-d00a-4956-a3de-c83e929730cd")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
 
 [assembly: AssemblyVersion("25.3.2")]
 [assembly: AssemblyFileVersion("25.3.2")]
